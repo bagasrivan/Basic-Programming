15041dd6ade704db6775a102be6f316cc2c3f766
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-13 14:50:52 +0800

    Add files via upload

---
+++ Soal1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bagaskara
+ */
+public class Soal1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Nama = ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukan NIM = ");
+        String NIM = input.nextLine();
+    }
+
+}
