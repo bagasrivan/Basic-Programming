65667324944d433c947b1f2652cffe4e671dd42e
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-13 14:58:00 +0800

    Add files via upload

---
+++ praktikum1/Soal2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bagaskara
+ */
+public class Soal2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Nama = ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukan NIM = ");
+        String NIM = input.nextLine();
+
+        System.out.println("Halo, Saya " + nama + " dengan NIM " + NIM + " dari program studi Ilmu Komputer. Salam Kenal.");
+
+    }
+
+}
