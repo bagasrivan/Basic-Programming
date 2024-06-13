cbee6d6e89f4130aa45a194efb3f439313b238d6
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-13 23:05:03 +0800

    Update case3.java

--- week4/case3.java
+++ week4/case3.java
@@ -1,40 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum4;
+package week4;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal3 {
+public class case3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String warna;
-        System.out.println("Program Menentukan Warna Primer");
+        String color;
+
+        System.out.println("Primary Color Determination Program");
         System.out.println("===============================");
 
-        System.out.print("Masukan Warna = ");
-        warna = input.nextLine();
+        System.out.print("Enter a Color = ");
+        color = input.nextLine();
 
-        switch (warna) 
-        {
-            case "Merah":
-            case "merah":
-            case "Biru":
-            case "biru":
-            case "Kuning":
-            case "kuning":
-                System.out.println(warna + " Adalah Warna Primer");
-            break;
-
+        switch (color.toLowerCase()) {
+            case "red":
+            case "blue":
+            case "yellow":
+                System.out.println(color + " is a Primary Color");
+                break;
             default:
-                System.out.println(warna + " Bukan Warna Primer");
-
+                System.out.println(color + " is not a Primary Color");
+                break;
         }
     }
-
 }
