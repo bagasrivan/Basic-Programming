1fe50acb133cfa490af2269f95f96fdc74f91e20
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-13 23:02:30 +0800

    Update case2.java

--- week4/case2.java
+++ week4/case2.java
@@ -1,43 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum4;
+package week4;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal2 {
+public class case2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String jawaban;
-        System.out.println("Soal :");
-        System.out.println("1. Hasil Dari 3 + 5 Adalah = ");
-        System.out.println("A. 7 \nB. 8 \nC. 9 \nD. 6 \nE. 10");
-        System.out.print("Jawaban Anda = ");
-        jawaban = input.nextLine();
+        String answer;
 
-        switch (jawaban) 
-        {
+        System.out.println("Question:");
+        System.out.println("1. What is the result of 3 + 5? ");
+        System.out.println("A. 7 \nB. 8 \nC. 9 \nD. 6 \nE. 10");
+        System.out.print("Your Answer = ");
+        answer = input.nextLine();
+
+        switch (answer.toUpperCase()) {
             case "A":
-            case "a":
             case "C":
-            case "c":
             case "D":
-            case "d":
             case "E":
-            case "e":
-                System.out.println("Jawaban Anda Salah");
+                System.out.println("Your answer is incorrect");
                 break;
             case "B":
-            case "b":
-                System.out.println("Jawaban Anda Benar");
+                System.out.println("Your answer is correct");
                 break;
             default:
-                System.err.println("Pilihan Jawaban Tidak Tersedia");
+                System.err.println("Invalid answer choice");
         }
     }
 }
