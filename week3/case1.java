2ba55776c2cd12010e3fcd1074ccea5c3e3d7b59
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-13 22:19:15 +0800

    Update case1.java

--- week3/case1.java
+++ week3/case1.java
@@ -1,36 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum3;
+package week3;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal1 {
+public class case1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-            int angka;
-            System.out.print("Masukan Angka = ");
-                angka = input.nextInt();
-            if (angka != 0){
-                if (angka % 10 == 0)
-                {
-                    System.out.println("Benar");
-                }
-                else if (angka == 0)
-                {
-                    System.out.println("Salah");
-                }
-               
+        int number;
+
+        System.out.print("Enter a Number = ");
+        number = input.nextInt();
+
+        if (number != 0) {
+            if (number % 10 == 0) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
             }
-            else 
-            {
-                System.out.println("Salah");
-            }
+        } else {
+            System.out.println("False");
+        }
     }
-    
 }
