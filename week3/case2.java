85c262c1e381e20450d27f81d997280ff9b249bf
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-13 22:21:48 +0800

    Update case2.java

--- week3/case2.java
+++ week3/case2.java
@@ -1,48 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum3;
+package week3;
+
 import java.util.Scanner;
-/**
- *
- * @author Bagaskara
- */
-public class Soal2 {
+
+public class case2 {
     public static void main(String[] args) {
-            double ip, sks;
-            System.out.println("Program Nilai IP");
-            Scanner input = new Scanner(System.in);
-        
-            System.out.print("Masukan IP = ");
-            ip = input.nextDouble();
-            
-            if (ip < 2.5)
-            {
-                System.out.println("Jatah SKS = 18");
-            }
-            
-            else if (ip < 2.75)
-            {
-                System.out.println("Jatah SKS = 20");
-            }
-            else if (ip < 3)
-            {
-                System.out.println("Jatah SKS = 22");
-            }
-            else if (ip < 3.5)
-            {
-                System.out.println("Jatah SKS = 23");
-            }    
-            else if (ip < 4)
-            {
-                System.out.println("Jatah SKS = 24");
-            }
-            else 
-            {
-                System.err.println("Sistem Error");
-            }
+        Scanner input = new Scanner(System.in);
+        double gpa, credits;
+
+        System.out.println("GPA Credit Program");
+
+        System.out.print("Enter GPA = ");
+        gpa = input.nextDouble();
+
+        if (gpa < 2.5) {
+            System.out.println("Credit Allocation = 18");
+        } else if (gpa < 2.75) {
+            System.out.println("Credit Allocation = 20");
+        } else if (gpa < 3) {
+            System.out.println("Credit Allocation = 22");
+        } else if (gpa < 3.5) {
+            System.out.println("Credit Allocation = 23");
+        } else if (gpa <= 4) { 
+            System.out.println("Credit Allocation = 24");
+        } else {
+            System.err.println("System Error");
+        }
     }
-    
 }
