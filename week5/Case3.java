f1302f1c27177d01f60d061728f96741b2627a18
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:11:40 +0800

    Update and rename case3.java to Case3.java

--- week5/Case3.java
+++ week5/Case3.java
@@ -1,6 +1,6 @@
 package week5;
 
-public class case3 {
+public class Case3 {
     public static void main(String[] args) {
         for (int i = 1; i <= 20; i++) {
             if (i % 3 == 0 && i % 6 == 0) {
