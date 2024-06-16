524ad27b320a32d1d5188fd4674eb6d719e22852
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 13:51:54 +0800

    Update case1.java

--- week5/case1.java
+++ week5/case1.java
@@ -1,39 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum5;
+package week5;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal1 {
+public class case1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int min = 999999, max = -9999999;
-        int totalAngka, angka;
-        int nilaiAwal = 0;
+        int totalNumbers, number;
+        int initialValue = 0;
 
-        System.out.print("Masukan Angka = ");
-        totalAngka = input.nextInt();
+        System.out.print("Enter the number of inputs: ");
+        totalNumbers = input.nextInt();
 
-        for (int j = 0; j < totalAngka; j++) {
-            System.out.print("Masukan Input Ke- " + (j + 1) + " : ");
-            angka = input.nextInt();
-            nilaiAwal += angka;
+        for (int j = 0; j < totalNumbers; j++) {
+            System.out.print("Enter Input " + (j + 1) + ": ");
+            number = input.nextInt();
+            initialValue += number;
 
-            if (angka > max) {
-                max = angka;
+            if (number > max) {
+                max = number;
             }
-            if (angka < min) {
-                min = angka;
+            if (number < min) {
+                min = number;
             }
         }
-        System.out.println("Nilai Terbesar Dari Inputan Adalah = " + max);
-        System.out.println("Nilai Terkecil Dari Inputan Adalah = " + min);
-        System.out.println("Nilai Rata-Rata Dari Inputan Adalah = " + (double) nilaiAwal / totalAngka);
+        System.out.println("The largest value from the inputs is: " + max);
+        System.out.println("The smallest value from the inputs is: " + min);
+        System.out.println("The average value from the inputs is: " + (double) initialValue / totalNumbers);
     }
 }
