7281110a7cf7e80ca20d7c0ff4f58f18e8504511
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:32:57 +0800

    Update Case4.java

--- week8/Case4.java
+++ week8/Case4.java
@@ -1,29 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum8;
+package week8;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal4 {
+public class Case4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n;
-        System.out.println("Program Menghitung Bilangan Fibbonaci");
+        
+        System.out.println("Fibonacci Number Calculation Program");
         System.out.print("Input n = ");
         n = input.nextInt();
-        System.out.println("Bilangan Fibbonaci ke-" + n + " Adalah = " + fibbo(n));
+        
+        System.out.println("Fibonacci number at position " + n + " is = " + fibonacci(n));
     }
 
-    public static int fibbo(int n) {
+    public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
         }
-        return fibbo(n - 1) + fibbo(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
