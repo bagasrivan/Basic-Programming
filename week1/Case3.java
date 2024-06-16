9cf773ceaa10c985e6f8f86e9a3634b65f36a12f
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:06:02 +0800

    Update and rename case3.java to Case3.java

--- week1/Case3.java
+++ week1/Case3.java
@@ -2,7 +2,7 @@ package week1;
 
 import java.util.Scanner;
 
-public class case3 {
+public class Case3 {
     public static void main(String[] args) {
         System.out.println("Hai, Nama Saya Bagaskara\nNIM 2211016310019\n\t Prodi Ilmu Komputer");
     }
