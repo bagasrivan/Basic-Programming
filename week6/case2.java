6f442073383827226e0b6126b0152e5529e5bd43
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:01:30 +0800

    Update case2.java

--- week6/case2.java
+++ week6/case2.java
@@ -1,37 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum6;
+package week6;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal2 {
+public class case2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int i = 0;
-        int angka;
-        double rataRata = 0;
+        int number;
+        double average = 0;
         int min = 999999, max = -999999;
-        String kondisi = "y";
+        String condition = "y";
 
-        System.out.println("Program Menghitung Rata-Rata Dan Nilai Terkecil Terbesar");
+        System.out.println("Program to Calculate Average, Minimum, and Maximum Values");
+        
         do {
-            System.out.print("Masukkan Angka = ");
-            angka = input.nextInt();
-            System.out.print("Ingin Dilanjutkan? (y/n) = ");
-            kondisi = input.next();
+            System.out.print("Enter a number = ");
+            number = input.nextInt();
+            System.out.print("Do you want to continue? (y/n) = ");
+            condition = input.next();
             i++;
-            rataRata = angka + rataRata;
-            max = (angka > max) ? angka : max;
-            min = (angka < min) ? angka : min;
-        } while (kondisi.equalsIgnoreCase("y"));
-        System.out.println("Rata-rata : " + (double) (rataRata / i));
-        System.out.println("Nilai Terkecil : " + min);
-        System.out.println("Nilai Terbesar : " + max);
+            average += number;
+            max = (number > max) ? number : max;
+            min = (number < min) ? number : min;
+        } while (condition.equalsIgnoreCase("y"));
+        
+        System.out.println("Average: " + (average / i));
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
     }
 }
