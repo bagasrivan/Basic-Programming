ce5ea74ff6e3c6d52b95efebea327eba94387894
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:08:43 +0800

    Update and rename case2.java to Case2.java

--- week3/Case2.java
+++ week3/Case2.java
@@ -2,7 +2,7 @@ package week3;
 
 import java.util.Scanner;
 
-public class case2 {
+public class Case2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double gpa, credits;
