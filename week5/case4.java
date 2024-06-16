8472f1629547d0a5462ada5eb07e0989bdb1f40a
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 13:56:12 +0800

    Update case4.java

--- week5/case4.java
+++ week5/case4.java
@@ -1,31 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum5;
+package week5;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal4 {
+public class case4 {
     public static void main(String[] args) {
-        long totalBilangan, nilaiAwal, selisih, fibonacci;
         Scanner input = new Scanner(System.in);
-        System.out.print("Masukan Banyak Bilangan Yang Ingin Ditampilkan = ");
-        totalBilangan = input.nextInt();
+        long totalNumbers, initialValue, difference, fibonacci;
+        System.out.print("Enter the number of Fibonacci terms to display: ");
+        totalNumbers = input.nextInt();
 
-        nilaiAwal = 1;
-        selisih = 0;
+        initialValue = 1;
+        difference = 0;
         fibonacci = 1;
 
-        for (int i = 1; i <= totalBilangan; i++) {
-            System.out.print(selisih + ", ");
-            fibonacci = nilaiAwal + selisih;
-            nilaiAwal = selisih;
-            selisih = fibonacci;
+        for (int i = 1; i <= totalNumbers; i++) {
+            System.out.print(difference + ", ");
+            fibonacci = initialValue + difference;
+            initialValue = difference;
+            difference = fibonacci;
         }
     }
 }
