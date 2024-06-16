18245b1ee26e6c5f6b8903ba59db15e03621a6b5
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:12:14 +0800

    Update and rename case1.java to Case1.java

--- week6/Case1.java
+++ week6/Case1.java
@@ -2,7 +2,7 @@ package week6;
 
 import java.util.Scanner;
 
-public class case1 {
+public class Case1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int menu;
