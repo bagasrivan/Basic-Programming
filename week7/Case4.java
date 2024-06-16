0440c0e747dbcc4533bb8efaae337f59f6afda36
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:25:20 +0800

    Update Case4.java

--- week7/Case4.java
+++ week7/Case4.java
@@ -1,109 +1,119 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum7;
+package week7;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal4 {
+public class Case4 {
     public static void main(String[] args) {
-        int pilihan;
         Scanner input = new Scanner(System.in);
+        int choice;
+        
         do {
-            System.out.println("Selamat Datang");
-            System.out.println("Menu : \n1.Input Radius dan Panjang Sisi \n2.Hitung Luas dan Keliling Lingkaran \n3.Hitung Luas dan Keliling Segitiga Sama Kaki \n4.Hitung Panjang L \n5.Keluar");
-            System.out.print("Masukan Pilihan = ");
-            pilihan = input.nextInt();
+            System.out.println("Welcome");
+            System.out.println("Menu: \n1. Input Radius and Side Length \n2. Calculate Area and Circumference of Circle \n3. Calculate Area and Perimeter of Isosceles Triangle \n4. Calculate Length L \n5. Exit");
+            System.out.print("Enter Your Choice = ");
+            choice = input.nextInt();
 
-            if (pilihan == 1) {
-                input();
+            if (choice == 1) {
+                inputValues();
                 System.out.println("====");
-            } else if (pilihan == 2) {
-                lingkaran();
+            } else if (choice == 2) {
+                circle();
                 System.out.println("====");
-            } else if (pilihan == 3) {
-                segitiga();
+            } else if (choice == 3) {
+                triangle();
                 System.out.println("====");
-            } else if (pilihan == 4) {
-
-            } else if (pilihan == 5) {
-                System.out.println("Anda Keluar Dari Program, Terimakasih");
+            } else if (choice == 4) {
+                calculateLengthL();
+                System.out.println("====");
+            } else if (choice == 5) {
+                System.out.println("You have exited the program. Thank you!");
             } else {
-                System.err.println("Pilihan Tidak Tersedia, Masukan Pilihan Yang Benar!!");
+                System.err.println("Invalid choice, please enter a valid option!!");
             }
-
-        } while (pilihan != 5);
-
+        } while (choice != 5);
     }
 
-    public static void input() {
+    public static void inputValues() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Silahkan Input Nilai Radius dan Panjang Sisi");
-        System.out.print("Masukan Nilai Radius = ");
+        
+        System.out.println("Please Input Radius and Side Length Values");
+        System.out.print("Enter Radius = ");
         int radius = input.nextInt();
-        System.out.print("Masukan Panjang Sisi = ");
-        int sisi = input.nextInt();
+        System.out.print("Enter Side Length = ");
+        int side = input.nextInt();
     }
 
-    public static void lingkaran() {
-        final double phi = 3.14;
+    public static void circle() {
+        Scanner input = new Scanner(System.in);
+        final double pi = 3.14;
         int r;
-        double hasil;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Selamat Datang Di Program Lingkaran");
-        System.out.println("Menu : \n1.Luas Lingkaran \n2.Keliling Lingkaran");
-        System.out.print("Masukan Pilihan Menu = ");
+        double result;
+        
+        System.out.println("Welcome to the Circle Program");
+        System.out.println("Menu: \n1. Circle Area \n2. Circle Circumference");
+        System.out.print("Enter Menu Choice = ");
         int menu = input.nextInt();
 
         if (menu == 1) {
-            System.out.println("Program Menghitung Luas Lingkaran");
-            System.out.print("Masukan Jari-Jari = ");
+            System.out.println("Program to Calculate Circle Area");
+            System.out.print("Enter Radius = ");
             r = input.nextInt();
-            hasil = phi * r * r;
-            System.out.println("Luas Lingkaran Adalah = " + hasil);
+            result = pi * r * r;
+            System.out.println("Circle Area = " + result);
         } else if (menu == 2) {
-            System.out.println("Program Menghitung Keliling Lingkaran");
-            System.out.print("Masukan Jari-Jari = ");
+            System.out.println("Program to Calculate Circle Circumference");
+            System.out.print("Enter Radius = ");
             r = input.nextInt();
-            hasil = 2 * phi * r;
-            System.out.println("Keliling Lingkaran Adalah = " + hasil);
+            result = 2 * pi * r;
+            System.out.println("Circle Circumference = " + result);
         } else {
-            System.err.println("Pilihan Tidak Tersedia");
+            System.err.println("Invalid choice");
         }
     }
 
-    public static void segitiga() {
-        int a, t, s;
-        double hasil;
+    public static void triangle() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Selamat Datang Di Program Segitiga Sama Kaki");
-        System.out.println("Menu : \n1.Luas Segitiga Sama Kaki \n2.Keliling Segitiga Sama Kaki");
-        System.out.print("Masukan Pilihan Menu = ");
+        int base, height, side;
+        double result;
+        
+        System.out.println("Welcome to the Isosceles Triangle Program");
+        System.out.println("Menu: \n1. Isosceles Triangle Area \n2. Isosceles Triangle Perimeter");
+        System.out.print("Enter Menu Choice = ");
         int menu = input.nextInt();
 
         if (menu == 1) {
-            System.out.println("Program Menghitung Luas Segitiga Sama Kaki");
-            System.out.print("Masukan Alas = ");
-            a = input.nextInt();
-            System.out.print("Masukan Tinggi = ");
-            t = input.nextInt();
-            hasil = a * t / 2;
-            System.out.println("Luas Segitiga Adalah = " + hasil);
+            System.out.println("Program to Calculate Isosceles Triangle Area");
+            System.out.print("Enter Base = ");
+            base = input.nextInt();
+            System.out.print("Enter Height = ");
+            height = input.nextInt();
+            result = base * height / 2.0;
+            System.out.println("Triangle Area = " + result);
         } else if (menu == 2) {
-            System.out.println("Program Menghitung Keliling Segitiga Sama Kaki");
-            System.out.print("Masukan Alas = ");
-            a = input.nextInt();
-            System.out.print("Masukan Sisi Yang Sama = ");
-            s = input.nextInt();
-            hasil = (2 * s) + a;
-            System.out.println("Keliling Segitiga Adalah = " + hasil);
+            System.out.println("Program to Calculate Isosceles Triangle Perimeter");
+            System.out.print("Enter Base = ");
+            base = input.nextInt();
+            System.out.print("Enter Equal Side Length = ");
+            side = input.nextInt();
+            result = (2 * side) + base;
+            System.out.println("Triangle Perimeter = " + result);
         } else {
-            System.err.println("Pilihan Tidak Tersedia");
+            System.err.println("Invalid choice");
         }
+    }
+
+    public static void calculateLengthL() {
+        Scanner input = new Scanner(System.in);
+        double a, b, c;
+        
+        System.out.println("Calculate Length L");
+        System.out.print("Enter Length a = ");
+        a = input.nextDouble();
+        System.out.print("Enter Length b = ");
+        b = input.nextDouble();
+        System.out.print("Enter Length c = ");
+        c = input.nextDouble();
+        double L = a + b + c;
+        System.out.println("The total length L is = " + L);
     }
 }
