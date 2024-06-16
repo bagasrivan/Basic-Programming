31c5b3dab5722ab2845e8d6de054e3b0f99e6847
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:05:46 +0800

    Update and rename case2.java to Case2.java

--- week1/Case2.java
+++ week1/Case2.java
@@ -2,7 +2,7 @@ package week1;
 
 import java.util.Scanner;
 
-public class case2 {
+public class Case2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
